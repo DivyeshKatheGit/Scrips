ee5d6451ea3a5bd3cb491ff917cfecf6db528322
DivyeshKatheGit <62831548+DivyeshKatheGit@users.noreply.github.com>
2021-03-17 13:57:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './css/App.css';
 import './scss/index.scss';
+import './css/App.css';
 import ScripsHeader from './components/Header/ScripsHeader';
 import ScripsBody from './components/Body/ScripsBody';
 import ScripsFooter from './components/Footer/ScripsFooter';
